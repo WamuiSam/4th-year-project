3b2551a53a37f3faefcef13195dff58491c8e06a
WamuiSam <samwelwamui@gmail.com>
2021-08-17 01:09:16 +0300

    firebase initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:wamui/routes/routes.gr.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   var _appRouter = AppRouter();
+  User? user = FirebaseAuth.instance.currentUser;
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
@@ -18,6 +24,7 @@ class MyApp extends StatelessWidget {
             textTheme: GoogleFonts.latoTextTheme(),
             primarySwatch: Colors.purple),
         routeInformationParser: _appRouter.defaultRouteParser(),
-        routerDelegate: _appRouter.delegate());
+        routerDelegate: _appRouter.delegate(
+            initialDeepLink: user != null ? '/home-page' : '/'));
   }
 }
