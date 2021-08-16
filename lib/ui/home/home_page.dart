6e209faf65490ba77fb1e2e7b79675fce4ad1a62
Samuel Baraka <sababuvercetti@gmail.com>
2021-08-17 01:19:50 +0300

    Home page design

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -7,8 +7,55 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CupertinoNavigationBar(
-        middle: Text("Home"),
+      body: Stack(
+        children: [
+          Positioned(
+              top: 100,
+              left: 20,
+              right: 20,
+              child: Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                          hintText: "Where to",
+                          enabledBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.purple, width: 2),
+                              borderRadius: BorderRadius.circular(5)),
+                          border: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.purple, width: 2),
+                              borderRadius: BorderRadius.circular(5))),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                          hintText: "From : My Location",
+                          enabledBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.purple, width: 2),
+                              borderRadius: BorderRadius.circular(5)),
+                          border: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.purple, width: 2),
+                              borderRadius: BorderRadius.circular(5))),
+                    ),
+                  ],
+                ),
+              )),
+          Positioned(
+              bottom: 50,
+              right: 20,
+              left: 20,
+              child: CupertinoButton(
+                  color: Colors.purple,
+                  child: Text("Check Traffick Info"),
+                  onPressed: () {}))
+        ],
       ),
     );
   }
