1939440bca73aac8c2b82f239f675d1e5d50a9e5
WamuiSam <samwelwamui@gmail.com>
2021-09-11 00:21:31 +0300

    create traffic service

---
+++ lib/services/traffic_services.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+import 'package:wamui/models/flow_segment_data.dart';
+
+class TrafficServices {
+  static Future<FlowSegmentData> getFlowSegmentData(
+      {required int versionNumber,
+      required String style,
+      required int zoom,
+      required double latitude,
+      required double longitude}) async {
+    Dio dio = Dio();
+    var response = await dio.get(
+        'https://api.tomtom.com/traffic/services/$versionNumber/flowSegmentData/relative0/$zoom/json');
+
+    return FlowSegmentData.fromJson(response.data['flowSegmentData']);
+  }
+}
